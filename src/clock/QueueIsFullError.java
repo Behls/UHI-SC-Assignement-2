92cebafff0cd0013ae56fd47bd10ef56a3114c22
Behls <alannazimbehl@gmail.com>
2022-08-02 01:53:37 +0100

    adding javadoc comments to error handling classes

--- src/clock/QueueIsFullError.java
+++ src/clock/QueueIsFullError.java
@@ -5,8 +5,9 @@
 package clock;
 
 /**
- *
- * @author dev
+ * This is an error handling class that handles full queues which extends the exception parent class, the constructor calls
+ * the parents constructor and handles the error by passing in a string message.
+ * @author Alanna Zimbehl - 19016467
  */
 public class QueueIsFullError extends Exception{
 
