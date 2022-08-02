2827c68cde1c635d8aa245edc035d54e1707d7de
Behls <alannazimbehl@gmail.com>
2022-08-02 01:51:02 +0100

    adding priority item class and error handling classes for the queue

---
+++ src/clock/QueueIsFullError.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clock;
+
+/**
+ *
+ * @author dev
+ */
+public class QueueIsFullError extends Exception{
+
+    public QueueIsFullError() {
+        super("Queue is full");
+        
+    }
+    
+    
+    
+}
